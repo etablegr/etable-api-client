7f0fe3c0f0cc783497ff2fa28103793f458df883
Yannis Rizos <yrizos@gmail.com>
2018-09-21 16:37:47 +0300

    added request timeout

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -7,43 +7,58 @@ use Psr\Http\Message\ResponseInterface;
 
 class ApiClient extends Client
 {
-    const DEFAULT_BASE_URI = 'https://api.e-table.gr';
-    const NAME             = 'yrizos/etable-api-client';
-    const VERSION          = '0.2';
-    const DEFAULT_LANGUAGE = 'en';
+    const BASE_URI = 'https://api.e-table.gr';
+    const NAME     = 'yrizos/etable-api-client';
+    const VERSION  = '0.2';
+    const LANGUAGE = 'en';
+    const TIMEOUT  = 30;
+
+    private $timeout = self::TIMEOUT;
 
     public function __construct(array $config = [])
     {
         $token    = isset($config['token']) ? $config['token'] : '';
-        $language = isset($config['language']) ? $config['language'] : self::DEFAULT_LANGUAGE;
+        $language = isset($config['language']) ? $config['language'] : self::LANGUAGE;
+        $timeout  = isset($config['timeout']) && is_numeric($config['timeout']) ? intval($config['timeout']) : self::TIMEOUT;
 
-        unset($config['token'], $config['language']);
+        unset($config['token'], $config['language'], $config['timeout']);
+
+        $this->setTimeout($timeout);
 
         if (!isset($config['base_uri'])) {
-            $config['base_uri'] = self::DEFAULT_BASE_URI;
+            $config['base_uri'] = self::BASE_URI;
         }
 
         $headers                    = isset($config['headers']) && is_array($config['headers']) ? $config['headers'] : [];
         $headers['User-Agent']      = self::getUserAgent();
         $headers['Authorization']   = 'Bearer ' . $token;
         $headers['Accept-Language'] = $language;
+        $headers['Accept']          = 'application/json';
 
         $config['headers'] = $headers;
 
         parent::__construct($config);
     }
 
-    public static function getUserAgent()
+    public function setTimeout(int $timeout): ApiClient
     {
-        return 'yrizos/etable-api-client/' . self::VERSION . ' (+https://gitlab.com/yrizos/etable-api-client)';
+        $this->timeout = $timeout;
+
+        return $this;
     }
 
-    public static function getArrayResponse(ResponseInterface $response)
+    public function getTimeout(): int
     {
-        $response = json_decode($response->getBody(), true);
-        $response = isset($response['data']) ? $response['data'] : [];
+        return $this->timeout;
+    }
 
-        return $response;
+    public function request($method, $uri = '', array $options = [])
+    {
+        if (!isset($options['timeout'])) {
+            $options['timeout'] = $this->getTimeout();
+        }
+
+        return parent::request($method, $uri, $options);
     }
 
     public function getNotifications(int $user_id): array
@@ -60,7 +75,7 @@ class ApiClient extends Client
         return self::getArrayResponse($response);
     }
 
-    final public function createUserSignedUpMessage(int $user_id)
+    public function createUserSignedUpMessage(int $user_id)
     {
         return $this->createNotification([
             'user_id' => $user_id,
@@ -68,7 +83,7 @@ class ApiClient extends Client
         ]);
     }
 
-    final public function createUserReviewUpvotedMessage(int $user_id, int $review_id, int $reviewer_id)
+    public function createUserReviewUpvotedMessage(int $user_id, int $review_id, int $reviewer_id)
     {
         return $this->createNotification([
             'user_id'     => $user_id,
@@ -78,7 +93,7 @@ class ApiClient extends Client
         ]);
     }
 
-    final public function createUserReviewReminderMessage(int $user_id, int $review_id)
+    public function createUserReviewReminderMessage(int $user_id, int $review_id)
     {
         return $this->createNotification([
             'user_id'   => $user_id,
@@ -87,7 +102,7 @@ class ApiClient extends Client
         ]);
     }
 
-    final public function createUserReviewFolloweeMessage(int $user_id, int $review_id)
+    public function createUserReviewFolloweeMessage(int $user_id, int $review_id)
     {
         return $this->createNotification([
             'user_id'   => $user_id,
@@ -96,7 +111,7 @@ class ApiClient extends Client
         ]);
     }
 
-    final public function createUserNpsReminderMessage(int $user_id)
+    public function createUserNpsReminderMessage(int $user_id)
     {
         return $this->createNotification([
             'user_id' => $user_id,
@@ -104,7 +119,7 @@ class ApiClient extends Client
         ]);
     }
 
-    final public function createUserLoyaltyPointsSummaryMessage(int $user_id, int $points)
+    public function createUserLoyaltyPointsSummaryMessage(int $user_id, int $points)
     {
         return $this->createNotification([
             'user_id' => $user_id,
@@ -113,7 +128,7 @@ class ApiClient extends Client
         ]);
     }
 
-    final public function createUserGotNewFollowerMessage(int $user_id, int $follower_id)
+    public function createUserGotNewFollowerMessage(int $user_id, int $follower_id)
     {
         return $this->createNotification([
             'user_id'     => $user_id,
@@ -122,7 +137,7 @@ class ApiClient extends Client
         ]);
     }
 
-    final public function createUser1000LoyaltyPointsMessage(int $user_id)
+    public function createUser1000LoyaltyPointsMessage(int $user_id)
     {
         return $this->createNotification([
             'user_id' => $user_id,
@@ -138,4 +153,16 @@ class ApiClient extends Client
         return self::getArrayResponse($response);
     }
 
+    public static function getUserAgent()
+    {
+        return 'yrizos/etable-api-client/' . self::VERSION . ' (+https://gitlab.com/yrizos/etable-api-client)';
+    }
+
+    public static function getArrayResponse(ResponseInterface $response)
+    {
+        $response = json_decode($response->getBody(), true);
+        $response = isset($response['data']) ? $response['data'] : [];
+
+        return $response;
+    }
 }
